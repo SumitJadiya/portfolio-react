175ccc65738a8aa432391613a8bdee6b6ed118d9
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-09-08 19:59:28 +0530

    added resume and other details

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,34 +3,32 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
 
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
+    if (this.props.data) {
+      var networks = this.props.data.social.map(function (network) {
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
     }
 
     return (
       <footer>
-
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">
               {networks}
-           </ul>
-
-           <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
-           </ul>
-
-           <ul className="copyright">
+            </ul>
+            <ul>
+              <li> Modified with love by STJ &nbsp;
+                <span role="img" aria-label="heart">♥️</span>
+              </li>
+            </ul>
+            <ul className="copyright">
               <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
-
+            </ul>
+          </div>
+          <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
+      </footer>
     );
   }
 }
