c7ec9cc7e487243600b240aec58a4f23f07c749f
Sumit Jadiya <jadiyaskj@gmail.com>
2021-10-21 11:56:18 +0530

    update url

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -16,15 +16,11 @@ class Footer extends Component {
             <ul className="social-links">
               {networks}
             </ul>
-            <ul>
+            {/* <ul>
               <li> Modified with love by STJ &nbsp;
                 <span role="img" aria-label="heart">♥️</span>
               </li>
-            </ul>
-            <ul className="copyright">
-              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-            </ul>
+            </ul> */}
           </div>
           <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
         </div>
