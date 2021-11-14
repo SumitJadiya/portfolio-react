c3b48d62423f248adeed1e9a53675a2284d5716e
Sumit Jadiya <sumitjadiya@Sumits-MacBook-Pro.local>
2021-11-14 17:02:21 +0530

    add corousal and update testimonials

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,32 +1,34 @@
 import React, { Component } from 'react';
 
 class Testimonials extends Component {
+   componentDidUpdate(){
+      test()
+   }
    render() {
-
+      
       if (this.props.data) {
          var testimonials = this.props.data.testimonials.map(function (testimonials) {
-            return <li key={testimonials.user}>
+            
+            return <div key={testimonials.user} className="slider slider-hidden">
                <blockquote>
-                  <p>{testimonials.text}</p>
+                  <p style={{whiteSpace: 'pre-line'}}>{testimonials.text}</p>
                   <cite>{testimonials.user}</cite>
                </blockquote>
-            </li>
+            </div>
          })
       }
-
+      
       return (
          <section id="testimonials">
             <div className="text-container">
-               <div className="row">
+               <div className="row testimonial-row">
 
                   <div className="two columns header-col">
                      <h1><span>LinkedIn Testimonials</span></h1>
                   </div>
 
-                  <div className="ten columns flex-container">
-                     <ul className="slides">
+                  <div className="ten columns">
                         {testimonials}
-                     </ul>
                   </div>
                </div>
             </div>
@@ -34,5 +36,23 @@ class Testimonials extends Component {
       );
    }
 }
+
+function test() {
+   // Custom
+const sliders = document.querySelectorAll(".slider");
+console.log(sliders)
+let current=0;
+sliders[current].classList.remove('slider-hidden')
+
+setInterval(()=>{
+    current=(current+1)%sliders.length;
+    for (let i=0; i<sliders.length; i++) {
+        sliders[i].classList.add('slider-hidden')
+        sliders[current].classList.remove('slider-hidden')
+    }
+}, 15000)
+
+}
+
 
 export default Testimonials;
